2ff5dd4096624a34d67ca042029ed438fca71599
joma74 <dev-mails@gmx.net>
2017-01-29 17:54:57 +0100

    Formatted code

--- episode11/main.js
+++ episode11/main.js
@@ -1,65 +1,69 @@
 Vue.component('tabs', {
-	props: [],
-	template: `
-	<div>
-		<div class="tabs">
-			<ul>
-			<li v-for="tab in tabs" :class="{ 'is-active': tab.isActive }">
-				<a :href="tab.href" @click="selectTab(tab)">{{ tab.name }}</a>
-			</li>
-			</ul>
-		</div>
-		<slot></slot>
-	</div>
-	`,
-	methods: { // This is a static component, but methods are stateless - so they can be an object property.
-		selectTab(selectedTab) {
-			this.tabs.forEach( tab => {
-				tab.isActive = (tab.name == selectedTab.name);
-			});
-		}
-	},
-	data() { // This is a static component, so to make a instance factory wrap it e.g. in a return.
-		return { // The "data" option should be a function that returns a per-instance value in component definitions.
-			tabs: []
-		}
-	},
-	mounted() {
-		console.log(this.$children);
-	},
-	created(){
-		this.tabs = this.$children;
-	}
+    props: [],
+    template: `
+    	<div>
+    		<div class="tabs">
+    			<ul>
+        			<li v-for="tab in tabs" :class="{ 'is-active': tab.isActive }">
+        				<a :href="tab.href" @click="selectTab(tab)">{{ tab.name }}</a>
+        			</li>
+    			</ul>
+    		</div>
+    		<slot></slot>
+    	</div>
+    `,
+    methods: { // This is a static component, but methods are stateless - so they can be an object property.
+        selectTab(selectedTab) {
+            this.tabs.forEach(tab => {
+                tab.isActive = (tab.name == selectedTab.name);
+            });
+        }
+    },
+    data() { // This is a static component, so to make a instance factory wrap it e.g. in a return.
+        return { // The "data" option should be a function that returns a per-instance value in component definitions.
+            tabs: []
+        }
+    },
+    mounted() {
+        console.log(this.$children);
+    },
+    created() {
+        this.tabs = this.$children;
+    }
 });
 
 Vue.component('tab', {
-	props: {
-		name: { required: true },
-		selected: { default: false }
-	},
-	template: `
-		<div v-show="this.isActive">
-			<slot></slot>
-		</div>
-	`,
-	data() {
-		return {
-			isActive: false
-		}
-	},
-	mounted() {
-		this.isActive = this.selected;
-	},
-	computed: {
-		href() {
-			return '#' + this.name.trim().toLowerCase().replace(/ /g, '-');
-		}
-	}
+    props: {
+        name: {
+            required: true
+        },
+        selected: {
+            default: false
+        }
+    },
+    template: `
+    		<div v-show="this.isActive">
+    			<slot></slot>
+    		</div>
+    `,
+    data() {
+        return {
+            isActive: false
+        }
+    },
+    mounted() {
+        this.isActive = this.selected;
+    },
+    computed: {
+        href() {
+            return '#' + this.name.trim().toLowerCase().replace(/ /g, '-');
+        }
+    }
 });
 
 new Vue({
-	el: '#root',
-	data: { // This is a global object, so object properties are initialized one time
-		showSome: true
-	}
+    el: '#root',
+    data: { // This is a global object, so object properties are initialized one time
+        showSome: true
+    }
 });
