eb42dd1c8c28cc02f3b59442ead5b471118874f8
joma74 <dev-mails@gmx.net>
2017-02-02 00:13:44 +0100

    Episode 14 added and named, template beautified

--- episode14-namedslots/main.js
+++ episode14-namedslots/main.js
@@ -1,26 +1,34 @@
-Vue.component('cname', {
-    props: {
-        one: {
-            required: true,
-            default: ''
-        }
-    },
+Vue.component('modal', {
+    props: {},
     template: `
-			<input :placeholder="this.theComputed" @blur="onChanged" required>
+    <div class="modal is-active">
+      <div class="modal-background"></div>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">
+            <slot name="title"></slot>
+          </p>
+          <button class="delete"></button>
+        </header>
+        <section class="modal-card-body">
+          <slot>
+            Default content here.
+          </slot>
+        </section>
+        <footer class="modal-card-foot">
+          <a class="button is-success">Save changes</a>
+          <a class="button">Cancel</a>
+        </footer>
+      </div>
+    </div>
 		`,
     methods: { // This is a static component, but methods are stateless - so they can be an object property.
-        onChanged() {
-            this.$emit('changed');
-        }
     },
     data() { // This is a static component, so to make a instance factory wrap it e.g. in a return.
         return { // The "data" option should be a function that returns a per-instance value in component definitions.
         }
     },
     computed: { // access like data
-        theComputed() {
-            return this.one + ' vue';
-        }
     },
     created() { // lifecycle hook see
         console.log('Created ' + this.$options.name);
@@ -33,11 +41,6 @@ Vue.component('cname', {
 new Vue({
     el: '#root',
     data: { // This is a global object, so object properties are initialized one time
-        doShow: true
     },
-    methods: {
-        toggle() {
-            this.doShow = !this.doShow;
-        }
-    }
+    methods: {}
 });
