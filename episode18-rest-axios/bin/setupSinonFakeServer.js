e8de5b0a29ff3e518ebacbfcdc2a98abb497abc6
joma74 <dev-mails@gmx.net>
2017-03-28 01:33:39 +0200

    Refined fake server

--- episode18-rest-axios/bin/setupSinonFakeServer.js
+++ episode18-rest-axios/bin/setupSinonFakeServer.js
@@ -2,20 +2,54 @@ var sinon = require("sinon");
 
 var fakeServer = sinon.fakeServer.create();
 fakeServer.respondImmediately = true;
-var response = {
+var fake_response = {
     projects: [{
-        key: "12345",
-        name: "Hey there",
-        description: "I am",
-        lastModifiedOn: "24.03.2017"
+        key: "1",
+        name: "Jörg",
+        description: "Papa",
+        lastModifiedOn: "20.03.2017"
+    }, {
+        key: "2",
+        name: "Gitti",
+        description: "Mama",
+        lastModifiedOn: "21.03.2017"
+    }, {
+        key: "3",
+        name: "Clara",
+        description: "Putziputz",
+        lastModifiedOn: "22.03.2017"
     }]
 }
-var onGET = function(xhr, id) {
+var onGET = function(xhr) {
     xhr.respond(200, {
             "Content-Type": "application/json"
         },
-        JSON.stringify(response))
+        JSON.stringify(fake_response))
+};
+var onDELETE = function(xhr, id) {
+    xhr.respond(200)
+};
+var onPUT = function(xhr) {
+    xhr.respond(200);
+    fake_response.projects.push({
+        key: fake_response.projects.length,
+        name: "Helga",
+        description: "Oma",
+        lastModifiedOn: "23.03.2017"
+    });
 };
 fakeServer.respondWith(
+    "GET",
+    /.*\/api\/projects/,
     onGET
+);
+fakeServer.respondWith(
+    "DELETE",
+    /.*\/api\/projects\/(\d+)/,
+    onDELETE
+);
+fakeServer.respondWith(
+    "PUT",
+    /.*\/api\/projects/,
+    onPUT
 );
