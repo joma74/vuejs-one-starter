d8eff1b2d699dfc70c08ddbe5b8edb30d69546d3
joma74 <dev-mails@gmx.net>
2017-05-11 14:44:49 +0200

    setup mocha chai

---
+++ episode-vuerouter-team/src/test/js/test-suite.spec.js
@@ -0,0 +1,12 @@
+import assert from 'assert';
+import {
+  expect, default as chai
+} from 'chai';
+
+describe('Some Feature', function() {
+  it('is working', function() {
+    chai.config.truncateThreshold = 0; // to show content of actual and expected array
+    chai.config.showDiff = true;
+    expect(1).to.equal(1);
+  })
+})
