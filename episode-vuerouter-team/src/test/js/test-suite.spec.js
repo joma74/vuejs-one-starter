ef6eb7bd1352ac72556fcd6e64592d5ff9d3c97c
joma74 <dev-mails@gmx.net>
2017-05-12 00:20:04 +0200

    Added chromedriver install

--- episode-vuerouter-team/src/test/js/test-suite.spec.js
+++ episode-vuerouter-team/src/test/js/test-suite.spec.js
@@ -1,12 +1,17 @@
-import assert from 'assert';
 import {
-  expect, default as chai
+  describe,
+  it
+} from 'mocha';
+
+import {
+  expect,
+  default as chai
 } from 'chai';
 
-describe('Some Feature', function() {
-  it('is working', function() {
+describe('Some Feature', function () {
+  it('is working', function () {
     chai.config.truncateThreshold = 0; // to show content of actual and expected array
     chai.config.showDiff = true;
     expect(1).to.equal(1);
-  })
-})
+  });
+});
