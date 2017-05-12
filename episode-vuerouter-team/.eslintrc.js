791ee4e51090ab0d9cf0a57145bcd9b80ecab040
joma74 <dev-mails@gmx.net>
2017-05-12 09:40:24 +0200

    added vue to linter plugins per doc

--- episode-vuerouter-team/.eslintrc.js
+++ episode-vuerouter-team/.eslintrc.js
@@ -12,7 +12,8 @@ module.exports = {
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
   extends: 'standard',
   plugins: [
-    'html'
+    'html',
+    'vue'
   ],
   // add your custom rules here
   'rules': {
