a9b8f19c17dab2e9b15e25fada9dee15318f1048
joma74 <dev-mails@gmx.net>
2017-05-22 17:04:06 +0200

    Remove moved Deferred

--- episode-vuerouter-team/src/test/js/utils/ChaiConfig.js
+++ episode-vuerouter-team/src/test/js/utils/ChaiConfig.js
@@ -2,7 +2,7 @@ import chai from 'chai';
 
 class ChaiConfig{
   setDefaults() {
-    chai.config.truncateThreshold = 0; // to show content of actual and expected array
+    chai.config.truncateThreshold = 0; // to show  content of actual and expected array
     chai.config.showDiff = true;
     chai.config.includeStack = true;
   }
