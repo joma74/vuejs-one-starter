f43bbde15af793d5ba429d9f9e76d19c948f3f57
joma74 <dev-mails@gmx.net>
2017-06-20 22:13:57 +0200

    Corrected Mix patch for babel config nearer

--- episode-vuerouter-team/patches/patch.donotusebabelrc.Mix.js
+++ episode-vuerouter-team/patches/patch.donotusebabelrc.Mix.js
@@ -1,6 +1,6 @@
---- node_modules/laravel-mix/src/Mix.js	2017-06-02 23:38:51.474965250 +0200
-+++ node_modules/laravel-mix/src/donotusebabelrc.Mix.js	2017-06-02 23:38:00.094967399 +0200
-@@ -94,14 +94,15 @@
+--- node_modules/laravel-mix/src/Mix.js.orig	2017-06-15 21:21:15.774546742 +0200
++++ node_modules/laravel-mix/src/Mix.js	2017-06-20 22:06:26.313905160 +0200
+@@ -94,14 +94,16 @@
       * Fetch the appropriate Babel config for babel-loader.
       */
      babelConfig() {
@@ -11,7 +11,7 @@
          if (this.react) {
              options.babel.presets.push('react');
          }
--        
+ 
 -        return '?' + JSON.stringify(options.babel);
 +        let optionsBabel = JSON.stringify(options.babel);
 +        // console.log(optionsBabel);
